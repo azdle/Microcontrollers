a32f40eb0faee7b9f9ec35fa4f31bb5756719371
Patrick Barrett <Pat@theUnlink.com>
2012-07-18 09:59:59 -0500

    Works

--- 2012-06-25 - C Examples/USART.c
+++ 2012-06-25 - C Examples/USART.c
@@ -7,12 +7,13 @@
 //######### Function Declarations ################
 void high_isr(void);
 void low_isr(void);
+void setup(void);
 
 //######### Variables ################
 #define BUFSIZE 16      /* power of two should compile efficiently */
 
-volatile unsigned char txfifo[BUFSIZE];
-volatile unsigned txrd=0, txwr=0;       /* read and write positions */
+volatile unsigned char txbuf;
+volatile unsigned char rxbuf;
 
 //######### Interrupts ################
 #pragma code high_isr_entry=8
@@ -30,14 +31,13 @@ void low_isr_entry(void){
 void high_isr(void){
 	if(PIR1bits.RCIF = 1){
 		//Receive byte
+		rxbuf = RCREG;
 	}
 	
 	if(PIR1bits.TXIF = 1){
 		//Trasnmit Byte
-		TXREG = readTxBuffer();
-	}
-	
-	
+		TXREG = rxbuf;
+	}	
 }
 
 #pragma interruptlow low_isr
@@ -47,29 +47,16 @@ void low_isr(void){
 
 //######### Functions ################
 
-void TX(char byteToSend){
-	/* write a byte into FIFO */
-	txfifo[txwr++] = byteToSend;
-}
-
-char readTxBuffer(){
-	//Read a byte from fifo
-	  return txfifo[txrd++];
-}
-
-char bytesInTxBuffer(){
-	return txwr - txrd;
-}
-
 void setup(){
-	TXSTA = 0b00100100;
-	RCSTA = 0b10100000;
+	TXSTA = 0b00100000;
+	RCSTA = 0b10010000;
 	BAUDCON = 0b00001000;
 	SPBRGH = 2437 >> 8;
 	SPBRG = 2437;
 	
-	PIR1bits.RCIE = 1;
-	PIR1bits.TXIE = 1;
+	PIE1bits.RCIE = 1;
+	PIE1bits.TXIE = 1;
+	INTCONbits.PEIE = 1;
 	INTCONbits.GIE = 1;
 }
 
@@ -77,7 +64,6 @@ void setup(){
 void main(void) { 
 	setup();
 	while(1)	{
-	
 	}
 }
 
